9554a902cec4acaebd7256581bcd2c24a6a075d9
ravi-singh-13 <52980667+ravi-singh-13@users.noreply.github.com>
2023-06-03 14:04:49 +0530

    Update categoryController.js
    change in import adress Categoy to category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,5 @@
-const Category = require("../models/Category");
-const Product = require("../models/Product")
+const Category = require("../models/category");
+const Product = require("../models/product")
 const asyncHandler = require("express-async-handler");
 const {body, validationResult} = require("express-validator");
 
